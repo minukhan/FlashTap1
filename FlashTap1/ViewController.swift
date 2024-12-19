5c5b2a414f14da773f826f7cb73b80e091200349
한민욱 <dnrals7929@naver.com>
2024-12-19 09:48:04 +0900

    Initial Commit

---
+++ FlashTap1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlashTap1
+//
+//  Created by 한민욱 on 12/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
